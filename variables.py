2e75d6e4d005a6b7b94d5dc3cbd1e2643b91232e
Daniel <megalobravo@gmail.com>
2024-07-14 09:16:32 -0600

    se agrega archivo sobre variables

---
+++ variables.py
@@ -0,0 +1,5 @@
+nombre = "Daniel" # variable de tipo String
+dead = 22 # variable tipo entero
+altura = 1.80 # variable tipo float
+adulto = True # variable tipo bool
+listas = [1,2,3,4,5,"a","b"] # variable tipo listas
